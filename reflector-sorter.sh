1c10e499d813ffd96c8ed3a2155ba5e93e2dcfe8
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2022-11-05 08:46:59 +0700

    adding save location

--- reflector-sorter.sh
+++ reflector-sorter.sh
@@ -1,1 +1,1 @@
-reflector --sort score --latest 75 --thread 4 --country Japan,Indonesia,Singapore --score 75 --protocol "http,https"
+reflector --sort score --latest 75 --thread 4 --country Japan,Indonesia,Singapore --score 75 --protocol "http,https" --save /etc/pacman.d/mirrorlist
