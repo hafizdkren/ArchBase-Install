5e35488ff63b683b3ac06f70eadad79e208bc77d
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2022-11-06 10:32:28 +0700

    Update filesystem-format.sh

--- filesystem-format.sh
+++ filesystem-format.sh
@@ -2,10 +2,10 @@ lsblk
 
 mkfs.fat -F 32 -n Boot /dev/sda1
 
-mkfs.btrfs -L System /dev/sda2
+mkfs.btrfs -f -L System /dev/sda2
 
-mkfs.btrfs -L Home /dev/sda3 
+mkfs.btrfs -f -L Home /dev/sda3 
 
-mkfs.btrfs -L Games /dev/sda4
+mkfs.btrfs -f -L Games /dev/sda4
 
 parted /dev/sda print all
