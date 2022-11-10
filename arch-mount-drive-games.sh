8766cc80c08a66172879c914ba31653ec1d82a3e
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2022-11-10 18:14:29 +0700

    fixing btrfs subvolume

--- arch-mount-drive-games.sh
+++ arch-mount-drive-games.sh
@@ -1,4 +1,4 @@
 mkdir -p /mnt/home/hafizdkren/Games
-mount /dev/sda4 /mnt/home/hafizdkren/Games
+mount -o rw,noatime,nodatacow,noautodefrag,compress=zstd:10,space_cache=v2,subvolid=5,subvol=@Games /dev/sda4 /mnt/home/hafizdkren/Games
 
 lsblk
--- arch-mount-drive.sh
+++ arch-mount-drive.sh
@@ -1,9 +1,14 @@
 mount /dev/sda2 /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@Games
+umount /mnt
+mount -o rw,noatime,compress=zstd:5,space_cache=v2,subvolid=5,subvol=@ /dev/sda2 /mnt
 
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 
 mkdir -p /mnt/home
-mount /dev/sda3 /mnt/home
+mount -o rw,noatime,nodatacow,noautodefrag,compress=zstd:5,space_cache=v2,subvolid=5,subvol=@home /dev/sda3 /mnt/home
 
 lsblk
